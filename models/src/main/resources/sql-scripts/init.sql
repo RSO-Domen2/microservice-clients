cd38fea20d9f80d9ec2d72922d6f032355b136b2
dv6526 <dv6526@student.uni-lj.si>
2023-01-08 13:24:49 +0100

    sql real coordinates

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,2 +1,2 @@
-INSERT INTO client_metadata (name, surname, lat, lng, created, password, username) VALUES ('Domen', 'Vilar', 10.0, 20.0, TIMESTAMP '2007-01-01T12:00:00Z', 'geslo', 'domen');
-INSERT INTO client_metadata (name, surname, lat, lng, created, password, username) VALUES ('Matej', 'Vilar', 11.0, 20.0, TIMESTAMP '2006-01-02T12:00:00Z', 'geslo', 'matej');
+INSERT INTO client_metadata (name, surname, lat, lng, created, password, username) VALUES ('Domen', 'Vilar', 46.050380, 14.468480, TIMESTAMP '2007-01-01T12:00:00Z', 'geslo', 'domen');
+INSERT INTO client_metadata (name, surname, lat, lng, created, password, username) VALUES ('Matej', 'Vilar', 46.0425, 14.4895, TIMESTAMP '2006-01-02T12:00:00Z', 'geslo', 'matej');
